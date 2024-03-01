5a875cdaa59475af9237887d614aacffb3ac3211
telegrambots0 <146921432+telegrambots0@users.noreply.github.com>
2024-03-01 16:06:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6305751172:AAG3f0lLXE-ivB1r3xP0VSvUfR0nKfURYfM")
-    API_ID = int(os.environ["API_ID", 29759544]
-    API_HASH = os.environ["API_HASH", "97030a871720998c6efc5debcce67aa4"]
-    AUTH_USERS = "6952370497"""
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7078184845:AAEHj8NIzn7-kGJXumQgAGVlhDnhrhf6uB8")
+    API_ID = int(os.environ["API_ID", 27815405]
+    API_HASH = os.environ["API_HASH", "4e70821cd2af3322f7cf2f2887e32821"]
+    AUTH_USERS = "6679714610"""
 
